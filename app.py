191858bc32ac0bbb502287911408da70325eb2c7
Steeeephen <stephen.ofarrell64@gmail.com>
2021-03-19 11:02:31 +0100

    (2.0.0): Pillow update

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 
 from flask import render_template
 
+from assets.classtest import LolTracker
+
 os.makedirs('output/templates', exist_ok = True)
 app = flask.Flask(__name__, template_folder='output/templates', static_folder="output")
 
@@ -15,7 +17,7 @@ def index():
     for output in directories:
         table_rows += f'<a href=/page/{output[:-5]}>{output[:-5]}</a><br>'
 
-    return table_rows
+    return render_template('home.html')
 
 @app.route('/page/<video>')
 def test(video):
