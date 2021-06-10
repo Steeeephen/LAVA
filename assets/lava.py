5addb33f9866c8a1e816957a29bd1dd973425ea2
Steeeephen <stephen.ofarrell64@gmail.com>
2021-06-11 00:44:53 +0200

    (2.0.2): Implement DNN for Timer and GPU support

--- assets/lava.py
+++ assets/lava.py
@@ -15,7 +15,7 @@ from numpy.linalg import norm
 
 class LAVA(GraphsOperator):
   def __init__(self):
-    self.__version__ = '2.0.1'
+    self.__version__ = '2.0.2'
     output_file = os.path.join(
       'output',
       'positions')
