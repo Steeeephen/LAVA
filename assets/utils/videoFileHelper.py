c106d950cc180ac47023718e8db90578497a0c42
Allan Cao <a33cao@uwaterloo.ca>
2022-10-08 16:38:02 -0400

    fix local video file handlging

--- assets/utils/videoFileHelper.py
+++ assets/utils/videoFileHelper.py
@@ -12,16 +12,17 @@ def clean_for_directory(video_title : str) -> str:
 
 def parse_local_files(url : str) -> dict or None:
     videos = {}
+    print(url)
     if os.path.isdir(url):
         for file in os.listdir(url):
-            if file.split('.')[-1] in VALID_VIDEO_FORMATS:
+            if file.split('.')[-1].upper() in VALID_VIDEO_FORMATS:
                 # get the file's name
                 video_name = clean_for_directory(file)
                 videos[video_name] = os.path.join(url, file)
             else:
                 print(f'Invalid file format: {file}')
     elif os.path.isfile(url):
-        if url.split('.')[-1] in VALID_VIDEO_FORMATS:
+        if url.split('.')[-1].upper() in VALID_VIDEO_FORMATS:
             video_name = clean_for_directory(url)
             videos[video_name] = url
             return videos
