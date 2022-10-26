12f2d0419d114b5ad67bfb292d07b88edff45c61
ste <stephen.ofarrell@team.bumble.com>
2022-10-26 01:16:16 +0100

    remove leftovers

--- lava_lol/setup.py
+++
@@ -1,7 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name='lava_lol',
-    version='3.0.0',
-    packages=setuptools.find_packages()
-)
--- lava_lol/tests/mac_test.py
+++
@@ -1,9 +0,0 @@
-from assets.lava import LAVA
-
-lava = LAVA()
-
-lava.execute(
-	url = '/Users/stephenofarrell/Downloads/lplol.mp4',
-	local = True,
-	minimap = True
-)
